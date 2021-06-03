1aa8940db1ff44798a075d23137014c72b9e3548
khasyahfr <59479368+khasyah-fr@users.noreply.github.com>
2021-06-03 20:47:54 +0700

    Finalized main.py

--- Cloud/main.py
+++ Cloud/main.py
@@ -7,6 +7,8 @@ from tensorflow.keras import models
 import pickle
 import numpy as np
 
+from google.cloud import storage
+
 def get_frame(video_path, hour=0, minute=0, second=0):
     video=cv2.VideoCapture(video_path)
 
@@ -60,25 +62,29 @@ def predict(frame, model, coordinates):
   frame_bersih = frame_bersih[50:y+height, :]
   return empty, frame_bersih
 
+client = storage.Client()
 
-# model_path = PUT MODEL PATH HERE
-model = load_model(model_path)
+# bucket_name = SET BUCKET NAME HERE USING CLOUD CLIENT
+bucket = client.bucket(bucket_name)
 
-# coordinates_path = PUT COORDINATES PATH HERE
-coordinates = load_box(coordinates_path)
+# model_blob = SET MODEL BLOB HERE USING CLOUD CLIENT
+model = load_model(model_blob)
+
+# coordinates_blob = SET COORDINATES BLOB HERE USING CLOUD CLIENT
+coordinates = load_box(coordinates_blob)
 
 
 app = Flask(__name__)
 api = Api(app)
 
-#IN-MEMORY DATASTORE FOR DEMONSTRATION PURPOSES, USE RELATIONAL DATABASE FOR YOUR APPLICATION :D
+#IN-MEMORY DATASTORE FOR MVP PURPOSES, USE RELATIONAL DATABASE FOR MORE SCALABLE APPLICATION
 locations = {
     "polrestabdl": {
         "name": "Polresta Bandar Lampung",
         "street": "Jalan Mayjen MT Haryono",
         "address": "Jl. Mayjen MT Haryono, Gotong Royong, Kec. Tj. Karang Pusat, Kota Bandar Lampung, Lampung 35119",
         "description": "Kantor Polresta Provinsi Bandar Lampung melayani laporan & pengaduan masyarakat. Buka 07.00-14.30.",
-        "available": 37,
+        "available": 0,
         "capacity": 53,
         "image_parkinglot": "",
         "image_thumbnail": "",
@@ -90,7 +96,7 @@ locations = {
         "street": "Jalan Malioboro",
         "address": "Jl. Malioboro, Sosromenduran, Gedong Tengen, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55271",
         "description": "Wisata Yogyakarta tujuan utama turis dari dalam dan luar negeri. Buka 24 jam.",
-        "available": 122,
+        "available": 0,
         "capacity": 250,
         "image_parkinglot": "",
         "image_thumbnail": "",
@@ -102,7 +108,7 @@ locations = {
         "street": "Jalan Margo Utomo",
         "address": "Jl. Margo Utomo, Sosromenduran, Gedong Tengen, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55271",
         "description": "Stasiun Kereta Api yang menjadi pusat transportasi di Yogyakarta. Buka 24 jam.",
-        "available": 58,
+        "available": 0,
         "capacity": 80,
         "image_parkinglot": "",
         "image_thumbnail": "",
@@ -111,7 +117,7 @@ locations = {
 }
 
 
-# ROUTING
+# Locations handler lists all available locations on the datastore/database
 class Locations(Resource):
     def get(self):
         places = []
@@ -123,12 +129,16 @@ class Locations(Resource):
         response = {"status": "success", "data": places}
         return jsonify(response)
 
+# Place Information handler list all the details on specified location.
+# available and image_parkinglot, the two fundamental practical use case of this application got their results from implemented Machine Learning model.
+# keep in mind both Malioboro and Stasiun Yogya use the same Video blob because the CCTV livestream in Indonesia are very limited thus we use the same one for MVP.
+# as more and more location streamed their parking lot, we can develop more business logic in the PlaceInformation handler.
+
 class PlaceInformation(Resource):
     def get(self, name):
         
-
-        # video_path = PUT YOUR VIDEO / IMAGE STREAM HERE  
-        frame = get_frame(video_path)
+        # video_blob = PUT YOUR VIDEO / IMAGE STREAM BLOB HERE USING CLOUD CLIENT
+        frame = get_frame(video_blob)
         
         place = locations[name]
         place["available"], place["image_parkinglot"] = predict(frame, model, coordinates)
